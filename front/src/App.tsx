f6a0f5efb9544e057e9bc1b0b6a05047149b39a3
Dong Han <lux00leo@gmail.com>
2025-10-12 07:13:45 +0900

    chore-FE: darkmode

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import {RouterProvider} from "react-router";
 import router from "@/routers/router.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ThemeProvider} from "@/contexts/ThemeProvider.tsx";
 
 export default function App() {
     const queryClient = new QueryClient({
@@ -13,7 +14,9 @@ export default function App() {
     });
     return (
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router}/>
+            <ThemeProvider defaultTheme="dark">
+                <RouterProvider router={router}/>
+            </ThemeProvider>
         </QueryClientProvider>
     );
 };
